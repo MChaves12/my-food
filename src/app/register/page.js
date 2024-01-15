331abfd4c8112dadc1e6fe3e65daeb3fb47e83a9
MATHEUS CHAVES ARAUJO <002919@cesed.local>
2024-01-15 10:35:05 -0300

    feat: handleFormSubmit logic

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,14 +1,33 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        fetch('/api/register', {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'}
+        });
+    }
+
     return (
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl mb-8"> 
                 Register 
             </h1>
-            <form className="block max-w-sm mx-auto">
-                <input type="email" placeholder="email" />
-                <input type="password" placeholder="password" />
+            <form className="block max-w-sm mx-auto" onSubmit={handleSubmit}>
+                <input type="email" placeholder="email" value={email}
+                        onChange={e => setEmail(e.target.value)}
+                />
+                <input type="password" placeholder="password" value={password}
+                        onChange={e => setPassword(e.target.value)}
+                />
                 <button type="submit"> Register </button>
                 <div className="my-4 text-center text-gray-400">
                     <p> or login with socials </p>
